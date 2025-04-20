77ee4ab02f1fa56f6f7d4bf9c5654e55915575e9
Craigmillion <choycraig@yahoo.com>
2025-04-21 00:48:46 +0800

    remove logging code

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ const testRouter = require("./routes/test.route");
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(req.ip);
-    next();
-})
-
 app.use("/test", testRouter);
 
 module.exports = app;
