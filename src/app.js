8f68bfeb880e7554b0433cd388a6e8ca23f18841
Craigmillion <choycraig@yahoo.com>
2025-04-21 00:43:37 +0800

    logging for requests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const testRouter = require("./routes/test.route");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(req.ip);
+    next();
+})
+
 app.use("/test", testRouter);
 
 module.exports = app;
